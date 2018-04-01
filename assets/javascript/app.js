d276c1519dee095bbf224e8dd6a943e06da72a4c
David Perez <david.perez@us.abb.com>
2018-04-01 14:55:11 -0400

    put limit number of bombs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,11 @@
 //array to store all the block objects in the mine_field container
 var array_of_blocks = [];
-var number_of_bombs = 0;
+var number_of_bombs = 0; //ten bombs
+var bomb_limit = 10;
 var number_of_flagged_blocks = 0;
 var block_bombs = [];
 var seconds = 120;
+var dimension = 9;
 
 //field generation function
 function generate_field() {
@@ -14,93 +16,102 @@ function generate_field() {
 	var temp_block_type = "";
 
 	//generate 81 blocks (9 x 9 grid)
-	for (var i = 1; i < 82; i++) {
+	for (var i = 1; i < dimension+1; i++) {
 		//set temporary block coordinates (to be overwritten every iteration of block creation loop)
-		temp_coordinates['x'] = index_x + 1;
-		temp_coordinates['y'] = index_y + 1;
-		//set temporary block type (will need some kind of algorithm to limit number of bombs
-		//and make sure they are evenly spread throughout the field)
-		temp_block_type = block_types[Math.floor(Math.random() * block_types.length)];
+		array_of_blocks[i] = {};
+		for (var j = 1; j < dimension+1; j++) {
+			temp_coordinates['x'] = index_x + 1;
+			temp_coordinates['y'] = index_y + 1;
+			//set temporary block type (will need some kind of algorithm to limit number of bombs
+			//and make sure they are evenly spread throughout the field)
+			// temp_block_type = block_types[Math.floor(Math.random() * block_types.length)];
+			temp_block_type = "empty";
+			//reference to the mine_field container
+			var mine_field = $('.mine_field');
+			//creating a new block element
+			var new_block_div = $('<div>');
 
-		//reference to the mine_field container
-		var mine_field = $('.mine_field');
-		//creating a new block element
-		var new_block_div = $('<div>');
+			//creating a new block object with default block_state and random block_type values
+			var new_block_object = {
+				block_state: "not_clicked",
+				block_type: temp_block_type,
+				block_coordinate_x: temp_coordinates['x'],
+				block_coordinate_y: temp_coordinates['y']
+			};
 
-		//creating a new block object with default block_state and random block_type values
-		var new_block_object = {block_state: "not_clicked",
-								block_type: temp_block_type,
-								block_coordinate_x: temp_coordinates['x'],
-								block_coordinate_y: temp_coordinates['y'],
-								block_adjacent_bombs: 0};
+			//push new blocks object to array
+			array_of_blocks[i][j] = new_block_object;
 
-		if (temp_block_type === "bomb")
-			block_bombs.push(new_block_object);
+			//set data-state, data-type, class & id attributes, and background image of the new block element
+			new_block_div
+				.data('state', new_block_object.block_state)
+				.attr('type', new_block_object.block_type)
+				.attr('id', `${temp_coordinates['x']}` + "-" + `${temp_coordinates['y']}`)
+				.addClass('block')
+				.css('background-image', `url(assets/images/${new_block_object.block_state}.png)`);
 
-		//push new blocks object to array
-		array_of_blocks.push(new_block_object);
+			//and append the new block element to the mine_field container
+			mine_field.append(new_block_div);
 
-		//set data-state, data-type, class & id attributes, and background image of the new block element
-		new_block_div
-			.data('state', new_block_object.block_state)
-			.data('type', new_block_object.block_type)
-			.attr('id', `${temp_coordinates['x']}` + "-" + `${temp_coordinates['y']}`)
-			.addClass('block')
-			.css('background-image', `url(assets/images/${new_block_object.block_state}.png)`);
+			//increment index_x
+			index_x++;
 
-		//and append the new block element to the mine_field container
-		mine_field.append(new_block_div);
-
-		//increment index_x
-		index_x++;
-
-		//unless index_x divided by 9 has a remainder of 0 (end of row)
-		if (index_x % 9 === 0) {
-			//then reset index_x and increment index_y (new row)
-			index_x = 0;
-			index_y++;
+			//unless index_x divided by 9 has a remainder of 0 (end of row)
+			if (index_x % 9 === 0) {
+				//then reset index_x and increment index_y (new row)
+				index_x = 0;
+				index_y++;
+			}
 		}
 	}
 
+	plantBombs(array_of_blocks, number_of_bombs, bomb_limit);
+
 	//testing purposes
-	//console.log(array_of_blocks);
+	// console.log(array_of_blocks);
+	// console.log(block_bombs);
 }
 
-//needs to optimize too many loops
-function countAdjacentMines() {
-	for (var i = 0; i < array_of_blocks.length; i++){
-		for (var j = 0; j < array_of_blocks.length; j++){
-			if (!(array_of_blocks[i][j].block_type.bomb)){
-				var count = 0;
+function getRandomNumber(max) {
+	var num = Math.floor((Math.random() * 1000) + 1) % max;
+	return (num === 0) ? getRandomNumber(max) : num; 
+}
 
-				for (var p = i - 1; p <= i + 1; p++){
-					for (var q = j - 1; q <= j + 1; q++){
-						if (0 <= p && p < array_of_blocks.length && 0 <= q && q < array_of_blocks.length){
-							if (array_of_blocks[p][q].block_type.bomb)
-								++count;
-						}
-					}
-				}
+function plantBombs(array, bombsPlanted, limit) {
 
-				array_of_blocks[i][j].block_adjacent_bombs(count);
-			}
-		} // end for loop rows
-	} // end for loop columns
-} // end countAdjacentMines
+ 	var index_x, index_y;
 
-function startGameTimer () {
+ 	while (bombsPlanted <= limit) {
+ 		index_x = getRandomNumber(9);
+		index_y = getRandomNumber(9);
+		var temp_obj = array[index_x][index_y];
+		if (temp_obj.block_type !== "bomb") {
+			temp_obj['block_type'] = "bomb";
+			temp_obj['block_coordinate_x'] = index_x;
+			temp_obj['block_coordinate_y'] = index_y;
+			//search for specific coordinates
+			$('#'+index_x + '-' + index_y).attr('type',temp_obj.block_type);
+			array[index_x][index_y] = temp_obj;
+			block_bombs[index_x] = {};
+			block_bombs[index_x][index_y] = temp_obj;
+			bombsPlanted++;
+		}
+ 	}
+}
+
+function startGameTimer() {
 	interval = setTimeout(startGameTimer, 1000);
 	seconds--;
 
 	console.log("Time Remaining: " + fancyTimeFormat(seconds));
 
-	if (seconds === 0) {    
+	if (seconds === 0) {
 		clearTimeout(interval);
 		console.log("Game Over!");
-	}   
+	}
 }
 
-function fancyTimeFormat(time) {   
+function fancyTimeFormat(time) {
 	// Hours, minutes and seconds
 	var hrs = ~~(time / 3600);
 	var mins = ~~((time % 3600) / 60);
@@ -119,8 +130,8 @@ function fancyTimeFormat(time) {
 }
 
 //when a div with the class 'block' is clicked on
-$(document).ready(function() {
-	$('body').on('mousedown', '.block', function(event) {
+$(document).ready(function () {
+	$('body').on('mousedown', '.block', function (event) {
 		console.log("block clicked");
 
 		switch (event.which) {
@@ -135,12 +146,11 @@ $(document).ready(function() {
 				//determine if we clicked block was bomb
 				//or if we should perform the empty block crawl
 				//example below:
-				if ($(this).data('type') === "bomb")
+				if ($(this).data('type') === "bomb") {
 					console.log("a bomb block was clicked!! - perform_bomb_clicked_method() " + "--- the bomb's coordinates were " + $(this).attr('id'));
-				else if ($(this).data('type') === "empty")
+				} else if ($(this).data('type') === "empty") {
 					console.log("an empty block was clicked - perform_the_empty_area_crawl_method() " + "--- the clicked block's coordinates were " + $(this).attr('id'));
-
-
+				}
 				break;
 			case 2:
 				//case for middle click (not used)
